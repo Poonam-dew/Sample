1c116c02ca633fcfe76143afcf471188385c6dc0
Poonam-dew <dewanganpoonam778@gmail.com>
2025-02-14 21:48:42 +0530

    commit

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import '../Styles/CourseCard.css';
-const CourseCard=({img ,heading ,subheading, info ,setModal_1 ,setButtonText1,buttonText1})=>{
-  return(
-       <div className="course_cards">
-        <div className="couse_card_item">
-          <img src={img} className="course_img"/>
-          <div className="course_card_info">
-            <h2 className='course_card_heading'>
-              {heading}
-            </h2>
-            <h3 className="course_card_subheading">{subheading}</h3>
-            <ul className="courselinks">
-              <li>{info[0]}</li>
-              <li>{info[1]}</li>
-              <li>{info[2]}</li>
 
-            </ul>
-            <div className="knowbtn" onClick={()=>{setModal_1(true)
-                setButtonText1(buttonText1 === 'KNOW MORE' ? 'CLOSE' : 'KNOW MORE');
-
-              }} >KNOW MORE</div>
-          </div>
+const CourseCard = ({
+  img,
+  heading,
+  subheading,
+  info,
+  setModal_1,
+  setButtonText1,
+  buttonText1,
+  handleModalClose
+}) => {
+  return (
+    <div className="course_card">
+      <img src={img} className="course_img" alt="Course" />
+      <div className="course_card_info">
+        <h2 className="course_card_heading">{heading}</h2>
+        <h3 className="course_card_subheading">{subheading}</h3>
+        <ul className="courselinks">
+          {info.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <div
+          className="knowbtn"
+          onClick={() => {
+            setModal_1(true);
+            setButtonText1(buttonText1 === 'KNOW MORE' ? 'KNOW MORE' : 'KNOW MORE');
+          }}
+        >
+          {buttonText1}
         </div>
-        
-      </div> 
-   
-      
-    )
-}
+      </div>
+    </div>
+  );
+};
+
 export default CourseCard;
--- src/components/CourseModal_1.js
+++ src/components/CourseModal_1.js
@@ -18,16 +18,16 @@ const CourseModal_1=({onClose , AddId })=>{
          <li>This will generate a confidence in the participants that with the help of various online tools they can read and understand the original Sanskrit texts.</li>
          <li>Learning will occur in two modes - Lectures and Project.</li>
          <li>Module-wise evaluation through Multiple Choice Questions (MCQ) will carry 70% weightage, while project work will have 30% weightage of the total evaluation.</li>
-         <li>For more details on the project work and the tentative duration/plan of the course, please refer to this document.<a href="https://www.dropbox.com/s/jr4l0ijuvkfmhea/Intro_to_Sa%E1%B9%83s%C4%81dhan%C4%AB-IITR.pdf?raw=1" target="_blank">Document</a></li>
+         <li>For more details on the project work and the tentative duration/plan of the course, please refer to this document.<a className='cli' href="https://www.dropbox.com/s/jr4l0ijuvkfmhea/Intro_to_Sa%E1%B9%83s%C4%81dhan%C4%AB-IITR.pdf?raw=1" target="_blank">Document</a></li>
        </ul>
        </div>
        <div className='material_wrapper_1'>
         <h2>COURSE MATERIALS</h2>
         <h3>Feel free to access all the course materials here...</h3>
         <ul className="material_links_1>">
-          <li><a href="https://drive.google.com/drive/folders/1RsbqGy9rd_1V_c-wEwIwbK0ayp249uRE?usp=sharing" target="_blank">Course study materials</a></li>
-           <li><a href="http://sanskrit.uohyd.ac.in/scl" target="_blank">Saṃsādhanī toolkit website (University server)</a></li> 
-             <li><a href="http://scl.samsaadhanii.in/scl" target="_blank">Saṃsādhanī toolkit website (Cloud)</a></li>
+          <li ><a className='cli' href="https://drive.google.com/drive/folders/1RsbqGy9rd_1V_c-wEwIwbK0ayp249uRE?usp=sharing" target="_blank">Course study materials</a></li>
+           <li ><a className='cli'href="http://sanskrit.uohyd.ac.in/scl" target="_blank">Saṃsādhanī toolkit website (University server)</a></li> 
+             <li ><a className='cli' href="http://scl.samsaadhanii.in/scl" target="_blank">Saṃsādhanī toolkit website (Cloud)</a></li>
           
         </ul>
         
